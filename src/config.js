955eafeab3330b544aed8349ded6eed194f41cd8
Ozzie Harb <ozharb@gmail.com>
2023-01-11 12:30:47 -0800

    updated database url

--- src/config.js
+++ src/config.js
@@ -1,8 +1,10 @@
-'use strict';
+"use strict";
 
 module.exports = {
   PORT: process.env.PORT || 8000,
-  NODE_ENV: process.env.NODE_ENV || 'development',
-  DATABASE_URL: process.env.DATABASE_URL || 'postgresql://dunder_mifflin@localhost/WEkend',
-  JWT_SECRET: process.env.JWT_SECRET || 'oz-special-jwt-secret',
+  NODE_ENV: process.env.NODE_ENV || "development",
+  DATABASE_URL:
+    process.env.DATABASE_URL ||
+    "postgres://xtogvimv:ATsFN_bzqioTUreRdNUJswhTv6aFoPVo@hansken.db.elephantsql.com/xtogvimv",
+  JWT_SECRET: process.env.JWT_SECRET || "oz-special-jwt-secret",
 };
